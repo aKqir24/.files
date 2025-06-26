f46c681e7440ef8513fa65ea2f3a1ef2f4539ae6
Akqir <akqir24@dell-optiplex-990.workspace>
2025-06-26 15:02:27 +0800

    Remove Some files

---
+++ setup.sh
@@ -0,0 +1,7 @@
+# Install all base packages
+# stow the dotfiles directory
+# Install pacstall package manager
+# Install python packages
+# Setup systemd-networkd
+# Build some packages
+# Run theming scripts
