30c852b5d6819690cfa4ab86ad9293cc14ac13c6
Akqir <akqir24@dell-optiplex-990.workspace>
2025-07-08 15:51:35 +0800

    Update i3 config and setup

--- .config/i3/start.sh
+++ .config/i3/start.sh
@@ -9,21 +9,16 @@ if [ "$1" = "audio" ]; then
 # launch the screenshot tool from call
 elif [ "$1" = "screenshot" ]; then 
 	FILENAME=$(date +"%Y-%m-%d_%H-%M-%S").png
-	IMAGE_DIR=$2$FILENAME
-	
-	if [ ! -z $WAYLAND_DISPLAY ]; then
-		pgrep -x slurp || 
-			grim -t png -l 9 -g "$(slurp)" -c $IMAGE_DIR
-		pgrep -x slurp && echo " " 
-	else
-		import $IMAGE_DIR
-	fi
+	IMAGE_DIR=$2$FILENAME	
  
 	function notifySRC() {
 		dunstctl close-all ;
 		notify-send "Screenshot taken..." \
 		"( $FILENAME ) has been saved!!"
 	}
+
+	gscreenshot -f $IMAGE_DIR --gui
+
 else
 	echo "Usage: sh start.sh"
 	echo "Program: audio, screenshot"
