a5da0771c6c81199fd5c90dc5e265d8c0001efed
Akqir <akqir24@dell-optiplex-990.workspace>
2025-07-08 16:20:31 +0800

    neovim todo plugin addedgit add --all

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -70,6 +70,15 @@ return {
 		require("barbecue.ui").toggle(true)
 		require("barbecue.ui").toggle()
 	end,
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    }
   }
 }
 
