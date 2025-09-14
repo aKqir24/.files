c517d0125d4cf90c859f6f1f6609aeea5bc59273
aKqir24 <Akqir@protonmail.com>
2025-09-15 00:22:55 +0800

    mpv & setup.sh changes

--- setup.sh
+++ setup.sh
@@ -1,25 +1,28 @@
-echo "Please be aware that this install script
+clear ; echo "setup: Please be aware that this install script
 is made on a debian mahine, so it might not work on most 
-distros not based on debian!" ; sleep 2
+distros that are not based on debian!" ; sleep 2
 
 # setup dotfiles directory and other dir
+echo "setup: Preparing & applying dotfiles"
 git clone https://github.com/aKqir24/.files.git
 cd $HOME/.files && stow . --adopt && cd $HOME
 git clone https://github.com/aKqir24/pywal16_scripts.git \
 	$HOME/.files/resources/scripts/
 
 # Install all base packages
+echo "setup: installing system packages"
 su root -c "apt update ;
 apt-get install picom i3-wm pipewire pipewire-pulse libssl-dev \
-				wireplumber rofi dunst xinit pipx clang gettext \
-				sudo celluloid alacritty viewnior libtool kdialog \
-				imagemagick xsettingsd nwg-look stow btop starship \
-				pcmanfm systemd-resolved neovim iwd  preload git ark \
+				wireplumber rofi dunst xinit pipx mpv automake \
+				sudo alacritty viewnior libtool kdialog imagemagick \
+				xsettingsd nwg-look stow btop starship pcmanfm clang \
+				systemd-resolved neovim iwd  preload git ark gettext \
 				fastfetch power-profiles-daemon fonts-noto-color-emoji \
 				libpulse-dev libsensors-dev libpipewire-0.3-dev libtool-bin \
-				autoconf automake libnotmuch-dev yq python3-gi python3-setuptools  
+				autoconf libnotmuch-dev yq python3-gi python3-setuptools 
 
 # setup systemd-networkd & services
+echo "setup: Configuring system"
 system_services=( systemd-networkd systemd-resolved
 				  power-profiles-daemon iwd )
 for service in ${system_service[@]}; do
@@ -32,20 +35,26 @@ systemctl --user enable --now wireplumber.service
 systemctl --user enable --now pipewire-pulse.service"
 
 # pacstall package manager and its available package
+echo "setup: Installing pacstall & pacstall packages..."
 sudo $( bash -c "$(curl -fsSL https://pacstall.dev/q/install)" &&
 		pacstall -I carla-git zen-browser rustdesk-deb i3status-rust rofi-emoji \
 		lmms-git dust-bin )
+
+echo "setup: Installing base packages"
 
 # install python packages
 pipx install pywal16
 
 # build some packages or clone some scripts
 git clone https://github.com/thenaterhood/gscreenshot.git
-cd gscreenshot && sudo $( pipx install . rm -r greenshot ; apt autoremove )
+cd gscreenshot && pipx install . && rm -rf greenshot ; sudo apt autoremove
 
+echo "setup: theme & icons are loading..."
 # Run theming scripts
 bash $HOME/.files/resources/scripts/pywal16_scripts/walsetup.sh --gui --verbose
 bash $HOME/.files/resources/scripts/pywal16_scripts/waloml.sh \
 	--alacritty --dunst --i3status-rs=~/.files/.config/i3/status/config.toml
 bash $HOME/.files/resources/scripts/pywal16_scripts/theming/rofi.sh
 source $HOME/.cache/wal/colors-tty.sh
+
+echo 'setup: Finished!!'
