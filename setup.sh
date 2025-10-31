9b120efb11827872f1b676a1bb3c136627d8b1b2
aKqir24 <Akqir@protonmail.com>
2025-10-31 18:32:20 +0800

    update(setup.sh): added some packages & tweaks

--- setup.sh
+++ setup.sh
@@ -5,18 +5,19 @@ distros that are not based on debian!" ; sleep 2
 # setup dotfiles directory and other dir
 echo "setup: Preparing & applying dotfiles"
 git clone --recurse-submodules https://github.com/aKqir24/.files.git
-cd $HOME/.files && stow . --adopt 
-stow -d resources/global.config/ -t ~ --adopt vscodium && cd $HOME
+cd "$HOME/.files" && stow . --adopt 
+stow -d resources/global.config/ -t ~ --adopt vscodium && cd "$HOME"
 
 # Setup thumbfast for mpv
-wget -P $HOME/.config/mpv/scripts/ \
+echo "Setting up Media..."
+wget -P "$HOME/.config/mpv/scripts/" \
 	https://raw.githubusercontent.com/po5/thumbfast/refs/heads/master/thumbfast.lua
-wget -P $HOME/.config/mpv/script-opts/ \
+wget -P "$HOME/.config/mpv/script-opts/" \
 	https://raw.githubusercontent.com/po5/thumbfast/refs/heads/master/thumbfast.conf
 
 # Install all base packages
 echo "setup: installing system packages"
-su root -c "apt update ;
+su root -c "apt update ;\
 apt-get install picom i3-wm pipewire pipewire-pulse libssl-dev \
 				wireplumber rofi dunst xinit pipx mpv automake \
 				sudo alacritty viewnior libtool kdialog imagemagick \
@@ -44,28 +45,24 @@ systemctl --user enable --now pipewire-pulse.service"
 
 # pacstall package manager and its available package
 echo "setup: Installing pacstall & pacstall packages..."
-sudo $( bash -c "$(curl -fsSL https://pacstall.dev/q/install)" &&
-		pacstall -I carla-git zen-browser i3status-rust rofi-emoji \
-		lmms-git dust-bin neovim-git )
+sudo $( bash -c "$(curl -fsSL https://raw.githubusercontent.com/aKqir24/pacstall/refs/heads/master/install.sh)" &&
+		pacstall -I gearlever-git zen-browser i3status-rust rofi-emoji bluetuith-bin lmms-git dust-bin neovim-git \
+		vscodium-bin winetricks-git mcpelauncher-ui-git )
 
+# installing other packages
 echo "setup: Installing base packages"
-
-# install python packages
 pipx install pywal16
-
-# build some packages or clone some scripts
 git clone https://github.com/thenaterhood/gscreenshot.git
 cd gscreenshot && pipx install . && rm -rf greenshot ; sudo apt autoremove
 
+# Run theming/icon scripts
 echo "setup: theme & icons are loading..."
-# Run theming scripts
 bash $HOME/.files/resources/scripts/pywal16_scripts/walsetup.sh --gui --verbose
 bash $HOME/.files/resources/scripts/pywal16_scripts/waloml.sh --alacritty \
 	--dunst --i3status-rs=~/.files/.config/i3/status/config.toml
 bash $HOME/.files/resources/scripts/pywal16_scripts/theming/rofi.sh
 source $HOME/.cache/wal/colors-tty.sh
 
-# Turn on BFQ I/O
+# Finilaize by Turning on BFQ I/O
 echo "bfq" | sudo tee /sys/block/sda/queue/scheduler
-
 echo 'setup: Finished!!'
