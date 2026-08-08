b2de3c547a060cdf306b74d5dd17fcbcaeebbea2
aKqir24 <Akqir@protonmail.com>
2026-08-08 13:16:51 +0800

    upd: gtk theme config

--- configs/sessions/xmonad/lib/Keys.hs
+++ configs/sessions/xmonad/lib/Keys.hs
@@ -5,9 +5,26 @@ import Data.List (isInfixOf)
 import XMonad.Actions.Navigation2D (Direction2D (..), windowGo, windowSwap)
 import XMonad.Actions.RotSlaves (rotAllUp, rotAllDown, rotSlavesUp)
 import XMonad.Actions.WithAll (sinkAll)
+import XMonad.Actions.Submap (submap)
 import XMonad.Layout.LayoutCombinators (JumpToLayout (..))
 import XMonad.Layout.ToggleLayouts (ToggleLayout (..))
 import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+import Graphics.X11
+
+resizeMap :: M.Map (KeyMask, KeySym) (X ())
+resizeMap = M.fromList
+    [ ((0, xK_h), sendMessage Shrink)
+    , ((0, xK_l), sendMessage Expand)
+    , ((0, xK_j), sendMessage Shrink)
+    , ((0, xK_k), sendMessage Expand)
+    , ((0, xK_Left), sendMessage Shrink)
+    , ((0, xK_Right), sendMessage Expand)
+    , ((0, xK_Down), sendMessage Shrink)
+    , ((0, xK_Up), sendMessage Expand)
+    , ((0, xK_Escape), return ())
+    , ((0, xK_Return), return ())
+    ]
 
 keyBinds :: [(String, X ())]
 keyBinds =
@@ -31,6 +48,8 @@ keyBinds =
   , ("M-<Right>", myRight)
   , ("M-<Up>", windowGo U False)
   , ("M-<Down>", windowGo D False)
+  , -- Resize Mode (i3 style)
+    ("M-r", submap resizeMap)
   , -- Layout switches
     ("M-S-f", sendMessage (JumpToLayout "Full"))
   , ("M-S-t", sendMessage (JumpToLayout "Tabbed"))
@@ -45,10 +64,10 @@ keyBinds =
   , ("M-<Space>", windows W.focusDown)
   , -- Run Programs
     ("M-e", spawn "pcmanfm")
+  , ("M-d", spawn "~/.config/rofi/launch.sh drun")
   , ("M-<Return>", spawn "alacritty")
   , ("M-<Print>", spawn "gscreenshot")
   , ("M1-<F4>", spawn "bash ~/.config/eww/toggle.sh powermenu")
-  , ("M-r", spawn "~/.config/rofi/launch.sh drun")
   , ("M-S-r", spawn "~/.config/rofi/launch.sh run")
   , ("M-.", spawn "~/.config/eww/toggle.sh emoji")
   ]
--- configs/sessions/xmonad/lib/Layouts.hs
+++ configs/sessions/xmonad/lib/Layouts.hs
@@ -6,7 +6,7 @@ import XMonad.Layout.Renamed (Rename (..), renamed)
 import XMonad.Layout.Spacing (Border (..), spacingRaw)
 import XMonad.Layout.Tabbed (tabbed, shrinkText)
 
-myLayouts theme = theGaps 4 (fullLayout ||| tallLayout ||| verticalLayout ||| tabbed shrinkText theme)
+myLayouts theme = theGaps 4 (tallLayout ||| fullLayout ||| verticalLayout ||| tabbed shrinkText theme)
   where
     fullLayout     = renamed [Replace "Full"] (Full :: Full Window)
     tallLayout     = renamed [Replace "Tall"] (Tall 1 (3 / 100) (1 / 2) :: Tall Window)
