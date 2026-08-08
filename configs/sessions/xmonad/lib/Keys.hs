132af4de9a4bcb03a75f76a4804e165b8bd454c5
aKqir24 <Akqir@protonmail.com>
2026-08-08 12:59:38 +0800

    upd: gtk theme config

--- configs/sessions/xmonad/lib/Keys.hs
+++ configs/sessions/xmonad/lib/Keys.hs
@@ -1,8 +1,9 @@
 module Keys (keyBinds) where
 
 import XMonad
+import Data.List (isInfixOf)
 import XMonad.Actions.Navigation2D (Direction2D (..), windowGo, windowSwap)
-import XMonad.Actions.RotSlaves (rotAllUp, rotSlavesUp)
+import XMonad.Actions.RotSlaves (rotAllUp, rotAllDown, rotSlavesUp)
 import XMonad.Actions.WithAll (sinkAll)
 import XMonad.Layout.LayoutCombinators (JumpToLayout (..))
 import XMonad.Layout.ToggleLayouts (ToggleLayout (..))
@@ -10,15 +11,31 @@ import qualified XMonad.StackSet as W
 
 keyBinds :: [(String, X ())]
 keyBinds =
+  let myLeft = withWindowSet $ \ws -> do
+        let lDesc = description . W.layout . W.workspace . W.current $ ws
+        if "Full" `isInfixOf` lDesc || "Tabbed" `isInfixOf` lDesc || "Tabs" `isInfixOf` lDesc
+          then windows W.focusUp
+          else windowGo L False
+      myRight = withWindowSet $ \ws -> do
+        let lDesc = description . W.layout . W.workspace . W.current $ ws
+        if "Full" `isInfixOf` lDesc || "Tabbed" `isInfixOf` lDesc || "Tabs" `isInfixOf` lDesc
+          then windows W.focusDown
+          else windowGo R False
+  in
   [ -- Window Manipulation
     ("C-q", kill)
   , ("M1-<Tab>", rotAllUp)
   , ("M1-S-<Tab>", rotSlavesUp)
   , -- Navigation
-    ("M-<Left>", windowGo L False)
-  , ("M-<Right>", windowGo R False)
+    ("M-<Left>", myLeft)
+  , ("M-<Right>", myRight)
   , ("M-<Up>", windowGo U False)
   , ("M-<Down>", windowGo D False)
+  , -- Layout switches
+    ("M-S-f", sendMessage (JumpToLayout "Full"))
+  , ("M-S-t", sendMessage (JumpToLayout "Tabbed"))
+  , ("M-S-w", sendMessage (JumpToLayout "Tall"))
+  , ("M-S-v", sendMessage (JumpToLayout "Vertical"))
   , -- i3-style swap + layout switch
     ("M-S-<Left>", windowSwap L False >> sendMessage (JumpToLayout "Tall"))
   , ("M-S-<Right>", windowSwap R False >> sendMessage (JumpToLayout "Tall"))
